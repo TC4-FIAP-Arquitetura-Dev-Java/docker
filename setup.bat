b066b3d936f82886a9d0c475741b2765eef9c25c
MVBPinho <marcos.vb.pinho@live.com>
2026-02-01 11:19:21 -0300

    Revert "Criação do projeto estrutura"
    This reverts commit 5d38ea2a73525a0e15ea00830dda4135d22f4c4a.

--- setup.bat
+++ setup.bat
@@ -19,9 +19,10 @@ if %ERRORLEVEL%==0 (
 )
 
 :: URLs dos repositórios
-set "REPO_LOGIN=https://github.com/Hackathon-Fiap-TC5/ms-login"
-set "REPO_USUARIO=https://github.com/Hackathon-Fiap-TC5/ms-usuario"
-set "REPO_AGENDAMENTO=https://github.com/Hackathon-Fiap-TC5/ms-agendamento"
+set "REPO_LOGIN=https://github.com/TC4-FIAP-Arquitetura-Dev-Java/ms-login"
+set "REPO_USUARIO=https://github.com/TC4-FIAP-Arquitetura-Dev-Java/ms-usuario"
+set "REPO_FEEDBACK=https://github.com/TC4-FIAP-Arquitetura-Dev-Java/ms-feedback"
+set "REPO_COLLETIONS=https://github.com/TC4-FIAP-Arquitetura-Dev-Java/collections"
 
 :: Clonar se os diretórios não existirem
 if not exist "ms-login" (
@@ -30,8 +31,11 @@ if not exist "ms-login" (
 if not exist "ms-usuario" (
     git clone %REPO_USUARIO%
 )
-if not exist "ms-agendamento" (
-    git clone %REPO_AGENDAMENTO%
+if not exist "ms-feedback" (
+    git clone %REPO_FEEDBACK%
+)
+if not exist "collections" (
+    git clone %REPO_COLLETIONS%
 )
 
 :: URL do docker-compose.yaml
